8a0cdef0b63c6858df43d52f838e97e1c6c2c4ce
zeteny12 <zetus0512@gmail.com>
2023-09-30 18:03:19 +0200

    szures 2.0

--- svájci_uj_oldal.js
+++ svájci_uj_oldal.js
@@ -1,50 +1,117 @@
 //Részletek az órákról
 //Passion-A8202
 document.getElementById('Passion-A8202-gomb').addEventListener('click', function() {
-    window.open('karóra_login.html', 'blank');
- });
+  window.open('karóra_login.html', 'blank');
+});
 
- //RoyalLady-10405
+//RoyalLady-10405
 document.getElementById('RoyalLady-10405-gomb').addEventListener('click', function() {
-   window.open('karóra_login.html', 'blank');
-  });
+  window.open('karóra_login.html', 'blank');
+});
 
-  //Sportive
-  document.getElementById('Sportive-gomb').addEventListener('click', function() {
-    window.open('karóra_login.html', 'blank');
-  });
+//Sportive
+document.getElementById('Sportive-gomb').addEventListener('click', function() {
+  window.open('karóra_login.html', 'blank');
+});
 
-  //LaPassion-10220
-  document.getElementById('LaPassion-10220-gomb').addEventListener('click', function() {
-    window.open('karóra_login.html', 'blank');
-  });
+//LaPassion-10220
+document.getElementById('LaPassion-10220-gomb').addEventListener('click', function() {
+  window.open('karóra_login.html', 'blank');
+});
 
-  //Classic-01002
-  document.getElementById('Classic-01002-gomb').addEventListener('click', function() {
-    window.open('karóra_login.html', 'blank');
-  });
+//Classic-01002
+document.getElementById('Classic-01002-gomb').addEventListener('click', function() {
+  window.open('karóra_login.html', 'blank');
+});
 
-  //Adventic
-  document.getElementById('Adventic-gomb').addEventListener('click', function() {
-    window.open('karóra_login.html', 'blank');
-  });
+//Adventic
+document.getElementById('Adventic-gomb').addEventListener('click', function() {
+  window.open('karóra_login.html', 'blank');
+});
 
 
 
 //Checkbox szűrés
 //Passion-A8202
-const checkboxPassion = document.getElementById('Passion-A8202-checkbox');
-const PassionA8202 = document.getElementById('Passion-A8202-kartya');
-checkboxPassion.addEventListener('change', function() {
-    if (checkboxPassion.checked) {
-        PassionA8202.style.display = 'flex';
+//Elemek, melyeket eltűnnek
+const RoyalLadyElem = document.querySelectorAll('#RoyalLady-10405-kartya');
+const SportiveElem = document.querySelectorAll('#Sportive-kartya');
+const LaPassionElem = document.querySelectorAll('#LaPassion-10220-kartya');
+const ClassicElem = document.querySelectorAll('#Classic-01002-kartya');
+const AdventicElem = document.querySelectorAll('#Adventic-kartya');
+
+//Passion-A8202-checkbox
+const szuroCheckbox = document.getElementById('Passion-A8202-checkbox');
+
+//Eseménykezelő hozzárendelése
+szuroCheckbox.addEventListener('change', function() {
+    const csakKijeloltElemek = szuroCheckbox.checked;
+
+    //Iterálunk az összes listaelemen
+    RoyalLadyElem.forEach(function(elem) {
+        const kategoria = elem.getAttribute('data-kategoria');
+
+        // Ha a Checkbox be van pipálva és az elem kategóriája nem megfelelő, elrejtjük az elemet
+        if (csakKijeloltElemek && kategoria !== 'kijelolt') {
+            elem.style.display = 'none';
+        } else {
+            elem.style.display = 'inline-flex';
+        }
+    });
+
+    SportiveElem.forEach(function(elem) {
+      const kategoria = elem.getAttribute('data-kategoria');
+
+      // Ha a Checkbox be van pipálva és az elem kategóriája nem megfelelő, elrejtjük az elemet
+      if (csakKijeloltElemek && kategoria !== 'kijelolt') {
+          elem.style.display = 'none';
+      } else {
+          elem.style.display = 'inline-flex';
+      }
+  });
+
+  LaPassionElem.forEach(function(elem) {
+    const kategoria = elem.getAttribute('data-kategoria');
+
+    // Ha a Checkbox be van pipálva és az elem kategóriája nem megfelelő, elrejtjük az elemet
+    if (csakKijeloltElemek && kategoria !== 'kijelolt') {
+        elem.style.display = 'none';
     } else {
-        PassionA8202.style.display = 'inline-flex';
+        elem.style.display = 'inline-flex';
     }
   });
 
+  ClassicElem.forEach(function(elem) {
+    const kategoria = elem.getAttribute('data-kategoria');
+
+    // Ha a Checkbox be van pipálva és az elem kategóriája nem megfelelő, elrejtjük az elemet
+    if (csakKijeloltElemek && kategoria !== 'kijelolt') {
+        elem.style.display = 'none';
+    } else {
+        elem.style.display = 'inline-flex';
+    }
+  });
+
+  AdventicElem.forEach(function(elem) {
+    const kategoria = elem.getAttribute('data-kategoria');
+
+    // Ha a Checkbox be van pipálva és az elem kategóriája nem megfelelő, elrejtjük az elemet
+    if (csakKijeloltElemek && kategoria !== 'kijelolt') {
+        elem.style.display = 'none';
+    } else {
+        elem.style.display = 'inline-flex';
+    }
+  });
+});
+
+
+
+
+
+
+
 //RoyalLady-10405
-const checkboxRoyalLady = document.getElementById('RoyalLady-10405-checkbox');
+/*const checkboxRoyalLady = document.getElementById('RoyalLady-10405-checkbox');
 const RoyalLady10405 = document.getElementById('RoyalLady-10405-kartya');
 checkboxRoyalLady.addEventListener('change', function() {
     if (checkboxRoyalLady.checked) {
@@ -52,10 +119,10 @@ checkboxRoyalLady.addEventListener('change', function() {
     } else {
         RoyalLady10405.style.display = 'inline-flex';
     }
-  });
+  });*/
 
 //Sportive
-const checkboxSportive = document.getElementById('Sportive-checkbox');
+/*const checkboxSportive = document.getElementById('Sportive-checkbox');
 const Sportive = document.getElementById('Sportive-kartya');
 checkboxSportive.addEventListener('change', function() {
     if (checkboxSportive.checked) {
@@ -63,7 +130,7 @@ checkboxSportive.addEventListener('change', function() {
     } else {
         Sportive.style.display = 'inline-flex';
     }
-  });
+  });*/
 
 //LaPassion-10220
 const checkboxLaPassion = document.getElementById('LaPassion-10220-checkbox');
